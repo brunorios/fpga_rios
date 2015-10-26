545df56cceae4daa53b6dec99de6db9531781bde
riosb1 <brunorios89@gmail.com>
2015-10-26 21:28:12 -0200

    Arquivo fonte do exercício 1a

---
+++ aula2/project_1/project_1.srcs/sources_1/new/exe1_a.vhd
@@ -0,0 +1,29 @@
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+
+-- Uncomment the following library declaration if using
+-- arithmetic functions with Signed or Unsigned values
+--use IEEE.NUMERIC_STD.ALL;
+
+-- Uncomment the following library declaration if instantiating
+-- any Xilinx leaf cells in this code.
+--library UNISIM;
+--use UNISIM.VComponents.all;
+
+entity exe1_a is
+    Port ( 
+        a_in1    : in std_logic;
+        b_in2    : in std_logic;
+        clk      : in std_logic;
+        cntr_int : in std_logic;
+        
+        out_b   : out std_logic
+    );
+end exe1_a;
+
+architecture Behavioral of exe1_a is
+
+begin
+
+
+end Behavioral;
