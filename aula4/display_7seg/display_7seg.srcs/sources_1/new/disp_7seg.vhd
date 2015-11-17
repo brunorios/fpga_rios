7993205216b98bdcf11bc50ee2fc3b4cf7a9b188
riosb1 <brunorios89@gmail.com>
2015-11-16 22:04:00 -0200

    Arquivo fonte do projeto do dispaly de 7 segmentos

---
+++ aula4/display_7seg/display_7seg.srcs/sources_1/new/disp_7seg.vhd
@@ -0,0 +1,45 @@
+LIBRARY IEEE;
+USE IEEE.STD_LOGIC_1164.ALL;
+use IEEE.numeric_std.all;
+
+entity SS_controller is
+generic(
+    fclk : natural := 100; -- frequencia do clk (Mhz)
+    f7s : natural := 100 -- frequencia de atualizacao dos displays (Hz)
+);
+port(
+    CLK : in STD_LOGIC;
+    btnCpuReset : in STD_LOGIC;
+    LED : out STD_LOGIC_VECTOR (15 downto 0);
+    SEG : out STD_LOGIC_VECTOR (6 downto 0);
+    AN : out STD_LOGIC_VECTOR (7 downto 0) := X"00";
+    dp : out STD_LOGIC
+);
+end SS_controller;
+
+ARCHITECTURE rtl OF SS_controller IS
+    -- Constantes que definem o valor mostrado no display
+    constant SS1_valor : integer range 0 to 9 := 2;
+    --constant SS1_valor : integer range 0 to 9 := 1;
+    --constant SS1_valor : integer range 0 to 9 := 0;
+    
+    signal seg_atual : std_logic_vector(6 downto 0);
+    
+BEGIN
+
+WITH SS1_valor select
+    seg <= "0111111" when 0, --0
+           "0000110" when 1, --1
+           "1101101" when 2, --2
+           "1001111" when 3, --3
+           "1100110" when 4, --4
+           "1101101" when 5, --5
+           "1111101" when 6, --6
+           "0000111" when 7, --7
+           "1111111" when 8, --8
+           "1110011" when others; --9
+           
+    seg <= not seg_atual;
+    an <= "11111110";
+           
+END rtl;
